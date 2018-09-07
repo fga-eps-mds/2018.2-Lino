107825696782e2072a94facbd44323279bb0fee4
Guilherme Marques <marquesguilherme3@gmail.com>
2018-09-07 01:50:21 -0300

    Implements pdf download feature

--- crawler/scraper.py
+++ crawler/scraper.py
@@ -1,4 +1,6 @@
 import scrapy
+import pdfx
+import json
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
@@ -10,3 +12,19 @@ class TheCrawler():
     def runCrawler(self):
         self.process.crawl('RU')
         self.process.start()  # the script will block here until the crawling is finished
+
+class JsonReader():
+    def __init__(self):
+        with open('result.json') as f:
+            self.body = json.load(f)
+
+class PdfReader():
+    def __init__(self):
+        self.data = JsonReader()
+    
+    def downloadMenu(self, campus):
+        data = self.data
+        for item in data.body:
+            if campus in item['text']:
+                pdf = pdfx.PDFx(item['url'])
+                pdf.download_pdfs('./downloads/')
