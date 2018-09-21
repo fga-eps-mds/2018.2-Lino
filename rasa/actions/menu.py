f91502c4a7775f9ef32fd606a15f1b202fafac35
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-09-21 12:08:17 -0300

    Requests actual weekday to service

--- rasa/actions/menu.py
+++ rasa/actions/menu.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from rasa_core.actions.action import Action
 from rasa_core.events import UserUtteranceReverted
 
@@ -8,10 +9,12 @@ class ActionDailyMenu(Action):
     
     def run(self, dispatcher, tracker, domain):
         period = tracker.get_slot('period')
-        # meal = tracker.get_slot('meal')
-        response = requests.get('https://dec5ffed.ngrok.io').json()
+        meal = tracker.get_slot('meal')
+        day = time.strftime('%A',time.localtime())
+        response = requests.get(
+            f'https://3cbf5110.ngrok.io/cardapio/{day}').json()
         print(period)
-        dispatcher.utter_message(response['ALMOÇO']['Salada:'])
+        dispatcher.utter_message(response['ALMOÇO'])
         return []
 
 class ActionWeeklyMenu(Action):
--- rasa/data/stories/stories.md
+++ rasa/data/stories/stories.md
@@ -1,3 +1,3 @@
 ## path_daily_meal
-* asks_daily_menu{"period": "hoje"}
+* asks_daily_menu{"period": "hoje", "meal": ""}
   - action_daily_menu
