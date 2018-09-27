5d5297a6a276fbf42c731720eb2c6115a901f5f1
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-09-27 12:48:10 -0300

    Connect to webcrawler and send to Users

--- rasa/notifier.py
+++ rasa/notifier.py
@@ -1,5 +1,7 @@
 import requests
+import time
 from pymongo import MongoClient
+from pprint import pprint
 from notification_config import db_user, db_password, telegram_token
 
 client = MongoClient(f'mongodb://{db_user}:{db_password}@ds239930.mlab.com:39930/lino_ru')
@@ -11,10 +13,38 @@ def getUsers():
     print(result)
     return result['users_list']
 
-def notify(text):
-    chats = getUsers()
-    text = text.replace(' ','+')
-    for id in chats:
-        requests.get(f'https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={id}&text={text}').json()
+def getMenu():
+    day = time.strftime('%A',time.localtime())
+    response = requests.get(f'http://localhost:5000/cardapio/{day}').json()
+    return response
 
-a = notify('Notification')
+def parseJson(res):
+    messages = []
+    messages.append('E hoje no RU nós teremos: ')
+    for item in res:
+        if item != 'DESJEJUM':
+            text = 'Para o ' + item.lower() + ':\n'
+        else:
+            text = 'Para o ' + 'café da manhã:' + '\n'
+        for sub in res[item]:
+            text += sub + ' ' + res[item][sub] + '\n'
+        messages.append(text)
+    return messages
+
+def notify(messages):
+    chats = getUsers()
+    for item in messages:
+        item = item.replace(' ', '+')
+    for id in chats:
+        for text in messages:
+            a = requests.get(
+                f'https://api.telegram.org/bot{telegram_token}/sendChatAction?chat_id={id}&action=typing').json()
+            pprint(a)
+            time.sleep(2)
+            a = requests.get(
+                f'https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={id}&text={text}').json()
+            pprint(a)
+
+res = getMenu()
+res = parseJson(res)
+a = notify(res)
