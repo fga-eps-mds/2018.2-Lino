190ae2071b8b2cc4dfd8d316ba1bb26da998e457
MatheusBlanco <msallesblanco@gmail.com>
2018-10-10 19:58:48 -0300

    Removes unused function and updates another
    Co-authored-by: MatheusBlanco <msallesblanco@gmail.com>
    Co-authored-bu: braga9 <braga.gabriel1000@gmail.com>

--- rasa/email_notifier.py
+++ rasa/email_notifier.py
@@ -7,43 +7,31 @@ from pymongo import MongoClient
 from pprint import pprint
 from notification_config import db_user, db_password, telegram_token
 
-client = MongoClient('mongodb://http://ds041831.mlab.com:41831/newAlert')
-db = client.239258843822-cgk0toiq0bjuga3213t1qm0vhgh5gjh2.apps.googleusercontent.com
 telegram_token = os.getenv('ACCESS_TOKEN', '')
 
-telegram_token = os.getenv('ACCESS_TOKEN', '')
-
-
-def getUsers():
-    notifications = db.notifications
-    result = notifications.find_one()
-    print(result)
-    return result['users_list']
-    
 def getEmail():
-    day = time.strftime('%A',time.localtime())
     response = requests.get('http://localhost:3000/newAlert').json()
     return response
 
 def parseJson(res):
     messages = []
-    messages.append('Vou ver se tem um e-mail pra você aqui: ')
     for item in res:
         if item.status == 200:
             text = 'Você tem um novo e-mail!' + '\n'
+            messages.append(text)
         elif item.status == 404:
             text = 'Parece que ninguém te mandou nada. Try again later' + '\n'
         else:
             text = 'Amigo, não to te achando aqui na lista de e-mail' + '\n'
         messages.append(text)
         return messages
+        for text in messages:
 
 def notify(messages):
     chats = getUsers()
     for item in messages:
         item = item.replace(' ', '+')
     for id in chats:
-        for text in messages:
             a = requests.get(
                 'https://api.telegram.org/bot{}/sendChatAction?chat_id={}&action=typing'.format(telegram_token, id)).json()
             time.sleep(1)
