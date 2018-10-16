5cc63d5c51a3c0817c5071851899042579d138ba
MatheusBlanco <msallesblanco@gmail.com>
2018-10-16 19:34:22 -0300

    Evolves parseJson method for notifier script and cronjob files
    Co-authored-by: MatheusBlanco <msallesblanco@gmail.com>
    Co-authored-by: braga9 <braga.gabriel1000@gmail.com>

--- rasa/email_notifier.py
+++ rasa/email_notifier.py
@@ -6,9 +6,9 @@ import time
 import os
 from pprint import pprint
 
-telegram_token = os.getenv('ACCESS_TOKEN', '659699316:AAHdYVzLxQnSJZMyGpfC5FnegYR16GNQzUA')
+telegram_token = os.getenv('ACCESS_TOKEN', '')
 
-URL = 'http://localhost:5003/newAlert'
+URL = 'https://5c64f69f.ngrok.io/newAlert'
 
 def getEmail():
     response = requests.get(URL).json()
@@ -19,19 +19,19 @@ def parseJson(response):
     if response == "Forbidden":
         message = 'Jovem, não to te achando aqui na lista de e-mail' + '\n'
     elif response == "No new messages found":
-        message = 'Parece que ninguém te mandou nada. Try again later' + '\n'
+        message = ""
     else:
-        message = response
+        message = 'Vocẽ recebeu e-mail. Dá uma olhada aí' + '\n' + '\n' + 'Remetente: ' + str(response['email']) + '\n' + 'Assunto: ' + str(response['subject']) + '\n' + 'Mensagem: ' + str(response['message'])
     return message
 
 def notify(message):
-    chats = []
+    chats = [216366978, 149140152]
     for id in chats:
         #    a = requests.get(
          #       'https://api.telegram.org/bot{}/sendChatAction?chat_id={}&action=typing'.format(telegram_token, id)).json()
           #  time.sleep(1)
-            a = requests.get(
-                'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(telegram_token, id, str(message))).json()
+        a = requests.get(
+            'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(telegram_token, id, str(message))).json()
 
 result = getEmail()
 response = parseJson(result)
