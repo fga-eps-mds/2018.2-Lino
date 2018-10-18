14c68dad5826923942ee32994219d0cdf6638314
MatheusBlanco <msallesblanco@gmail.com>
2018-10-18 17:56:47 -0300

    Fixes code in accordance to Flake8
    Co-authored-by: MatheusBlanco <msallesblanco@gmail.com>
    Co-authored-by: braga9 <braga.gabriel1000@gmail.com>

--- rasa/email_notifier.py
+++ rasa/email_notifier.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 import requests
-import urllib, json
 import os
 import time
-import os
-from pprint import pprint
 
 telegram_token = os.getenv('ACCESS_TOKEN', '')
 
-URL = 'https://5c64f69f.ngrok.io/newAlert'
+URL = os.getenv('WEBHOOK_URK', '')
+
 
 def getEmail():
     response = requests.get(URL).json()
     return response
+
 
 def parseJson(response):
     message = ""
@@ -21,18 +20,28 @@ def parseJson(response):
     elif response == "No new messages found":
         message = ""
     else:
-        message = 'Vocẽ recebeu e-mail. Dá uma olhada aí' + '\n' + '\n' + 'Remetente: ' + str(response['email']) + '\n' + 'Assunto: ' + str(response['subject']) + '\n' + 'Mensagem: ' + str(response['message'])
+        message = 'Vocẽ recebeu e-mail. Dá uma olhada aí' + '\n' + '\n' + \
+                'Remetente: ' + str(response['email']) + '\n' + 'Assunto: ' + \
+                str(response['subject']) + '\n' + 'Mensagem: ' + \
+                str(response['message'])
     return message
 
+
 def notify(message):
-    chats = [216366978, 149140152]
+    chats = []
     for id in chats:
-        #    a = requests.get(
-         #       'https://api.telegram.org/bot{}/sendChatAction?chat_id={}&action=typing'.format(telegram_token, id)).json()
-          #  time.sleep(1)
-        a = requests.get(
-            'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(telegram_token, id, str(message))).json()
+        requests.get(
+            'https://api.telegram.org/bot{}/sendChatAction?chat_id='
+            .format(telegram_token, id) +
+            '{}&action=typing'.format(id)).json()
+
+        time.sleep(1)
+
+        requests.get(
+            'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'
+            .format(telegram_token, id, str(message))).json()
+
 
 result = getEmail()
 response = parseJson(result)
-notify(response)
+notify(str(response))
