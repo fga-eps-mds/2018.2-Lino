5f1993aa8aa56c0668405172ef3df4bec5bcf886
Guilherme Augusto <guilherme.francais@gmail.com>
2018-10-31 15:03:46 -0300

    Creates method to send weekly menu to facebook users
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Guilherme Marques <marquesguilherme3@gmail.com>

--- rasa/week_notifier.py
+++ rasa/week_notifier.py
@@ -44,12 +44,65 @@ def build_filters():
 
 def get_weekly_menu():
 
-    try:
-        response = requests.get(
-            'https://webcrawler-ru.lappis.rocks/cardapio/pdf'
-        ).json()
-    except ValueError:
-        logging.warning('Decoding JSON has failed')
-        response = None
+    response = {
+        'status': 'ok',
+        'url': "https://ru.unb.br/images/Cardapio/outubrorefeitorio2018/" +
+        "FUPFGAFCE29-10-a-04-11.pdf"
+    }
+
+    # try:
+    #     response = requests.get(
+    #         'https://webcrawler-ru.lappis.rocks/cardapio/pdf'
+    #     ).json()
+    # except ValueError:
+    #     logging.warning('Decoding JSON has failed')
+    #     response = None
 
     return response
+
+
+def notify_daily_meal_to_facebook(message):
+    chats = get_facebook_users('week meal')
+
+    for chat in chats:
+        builded_message = build_facebook_message(
+            chat['sender_id'], message['url']
+        )
+
+        postfields = urlencode(builded_message)
+
+        url = get_url_facebook_parameter()
+
+        curl = pycurl.Curl()
+        curl.setopt(curl.URL, url)
+        curl.setopt(curl.POSTFIELDS, postfields)
+        curl.perform()
+        curl.close()
+
+
+def build_facebook_message(sender_id, url):
+    return {
+        "recipient": {
+            "id": sender_id
+        },
+        "message": {
+            "attachment": {
+                "type": "file",
+                "payload": {
+                    "url": url
+                }
+            }
+        }
+    }
+
+
+def get_url_facebook_parameter():
+    return ('https://graph.facebook.com/v2.6/{}/messages?access_token={}'
+            .format(PSID, PAGE_ACCESS_TOKEN))
+
+
+message = get_weekly_menu()
+
+if message:
+    notify_daily_meal_to_telegram(message)
+    notify_daily_meal_to_facebook(message)
