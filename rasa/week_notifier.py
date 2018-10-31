5adc1a043890e5dec72e1a703a665a819b5844d5
Guilherme Augusto <guilherme.francais@gmail.com>
2018-10-31 15:03:46 -0300

    Creates method to send weekly menu to facebook users
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Guilherme Marques <marquesguilherme3@gmail.com>

--- rasa/week_notifier.py
+++ rasa/week_notifier.py
@@ -53,3 +53,50 @@ def get_weekly_menu():
         response = None
 
     return response
+
+
+def notify_daily_meal_to_facebook(message):
+    chats = get_facebook_users('week meal')
+
+    for chat in chats:
+        builded_message = build_facebook_message(
+            chat['sender_id'], message['url']
+        )
+
+        postfields = urlencode(builded_message)
+
+        url = get_url_facebook_parameter()
+
+        curl = pycurl.Curl()
+        curl.setopt(curl.URL, url)
+        curl.setopt(curl.POSTFIELDS, postfields)
+        curl.perform()
+        curl.close()
+
+
+def build_facebook_message(sender_id, url):
+    return {
+        "recipient": {
+            "id": sender_id
+        },
+        "message": {
+            "attachment": {
+                "type": "file",
+                "payload": {
+                    "url": url
+                }
+            }
+        }
+    }
+
+
+def get_url_facebook_parameter():
+    return ('https://graph.facebook.com/v2.6/{}/messages?access_token={}'
+            .format(PSID, PAGE_ACCESS_TOKEN))
+
+
+message = get_weekly_menu()
+
+if message:
+    notify_daily_meal_to_telegram(message)
+    notify_daily_meal_to_facebook(message)
