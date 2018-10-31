fdd488fa989e3672f989213ed6e48d4e34537b91
Guilherme Augusto <guilherme.francais@gmail.com>
2018-10-31 15:00:19 -0300

    Gets all registered facebook users that wants week menu
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Guilherme Marques <marquesguilherme3@gmail.com>

---
+++ rasa/week_notifier.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import requests
+import os
+import time
+import pycurl
+import logging
+from urllib.parse import urlencode
+from pymongo import MongoClient
+
+# If you want to use your own bot to development add the bot token as
+# second parameters
+telegram_token = os.getenv('ACCESS_TOKEN', '')
+PAGE_ACCESS_TOKEN = os.getenv('PAGE_ACCESS_TOKEN', '')
+PSID = os.getenv('PSID', '')
+
+
+def get_facebook_users(message):
+    client = MongoClient('mongodb://mongo_facebook:27011/lino_facebook')
+    db = client['lino_facebook']
+
+    users = db.users.find(
+        build_query(message),
+        build_filters()
+    )
+
+    return users
+
+
+def build_query(message):
+    return {
+        'notification': {
+            'description': message,
+            "value": True
+        }
+    }
+
+
+def build_filters():
+    return {
+        '_id': 0,
+        'sender_id': 1
+    }
