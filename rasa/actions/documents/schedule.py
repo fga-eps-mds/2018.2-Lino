3ebf57ba6da825f545e4a22e3fb76473bd2e5372
pedro-prp <peddroprp@gmail.com>
2018-11-13 22:25:17 -0200

    Create all steps for tutorial
    Co-authored-by: MatheusBlanco <msallesblanco@gmail.com>
    Co-authored-by: gabrielfilipe7unb <gabrielfilipe7unb@gmail.com>
    Co-authored by: leticiadesouza <leticia.souza212@gmail.com>
    Co-authored-by: pedro-prp <peddroprp@gmail.com>

--- rasa/actions/documents/schedule.py
+++ rasa/actions/documents/schedule.py
@@ -4,7 +4,7 @@ DOC_1 = 'login/index.html?response_type=code&'
 DOC_2 = 'client_id=102&redirect_uri=/documentodigital/index.html'
 UNB_URL = f'https://servicos.unb.br/dados/{DOC_1}{DOC_2}'
 GIT_URL = 'https://raw.githubusercontent.com/fga-eps-mds/2018.2-Lino/'
-IMGS_PATH = ''
+IMGS_PATH = 'Issue_204-HourGridStatementFlow/rasa/images/schedule/'
 
 
 class ActionSchedule(Action):
@@ -22,6 +22,40 @@ class ActionSchedule(Action):
         for message in messages:
             dispatcher.utter_message(message)
 
+        steps = []
         
+        step_1 = {
+            'text': 'Passo 1: Faça login no site.',
+            'image': f'{GIT_URL}{IMGS_PATH}step1.png'
+            }
+        steps.append(step_1)
 
+        step_2 = {
+            'text': 'Passo 2: Selecione grade horária',
+            'image': f'{GIT_URL}{IMGS_PATH}step2.png'
+        }
+        steps.append(step_2)
+
+        step_3 = {
+            'text': 'Passo 3: Prove que você está ciente do que está fazendo',
+            'image': f'{GIT_URL}{IMGS_PATH}step3.png'
+        }
+        steps.append(step_3)
+
+        step_4 = {
+            'text': 'Passo 4: Clique em emitir',
+            'image': f'{GIT_URL}{IMGS_PATH}step4.png'
+        }
+        steps.append(step_4)
+
+        step_5 = {
+            'text': 'Passo 5: Agora é so baixar',
+            'image': f'{GIT_URL}{IMGS_PATH}step5.png'
+        }
+        steps.append(step_5)
+
+        for step in steps:
+            dispatcher.utter_response(step)
+        
+        
         return []
